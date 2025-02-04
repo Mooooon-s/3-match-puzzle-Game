ed7c18b0108fd857ccc1d25fcdb8609cbec42505
Mooooon-s <mmk990212@naver.com>
2025-02-04 12:50:30 +0900

    add asset

---
+++ 3 match puzzle game/Assets/Script/GridManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    public static GridManager Instance = null;
+    
+    public GameObject tile;
+    public int xSize,ySize;
+    public GameObject[,] tiles;
+
+    void Awake()
+    {
+        Instance = GetComponent<GridManager>();
+        Vector2 offset = tile.GetComponent<SpriteRenderer>().bounds.size;
+        Debug.Log(offset);
+        CreateGrid(offset.x,offset.y);
+    }
+
+    private void CreateGrid(float offsetX, float offsetY)
+    {
+        tiles = new GameObject[xSize, ySize];
+
+        for(int i = 0; i < xSize; i++)
+        {
+            for(int j = 0; j < ySize; j++)
+            {
+                GameObject NewTile = 
+                    Instantiate<GameObject>(tile, new Vector3(i*(i-offsetX),j*(j-offsetY)),tile.transform.rotation);
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
