ef141d4ca7f832c1683e88d8f3724681ec203366
Mooooon-s <mmk990212@naver.com>
2025-02-06 03:03:41 +0900

    FEAT: 블럭 랜덤으로 생성

---
+++ 3 match puzzle game/Assets/Script/MoveableBlock.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveableBlock : MonoBehaviour
+{
+    private GameBlock block;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        block = GetComponent<GameBlock>();   
+    }
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Move(float _mX,float _mY)
+    {
+        //index??
+        block.X = _mX;
+        block.Y = _mY;
+
+        Vector2 offset = block.GridRef.offSet;
+        block.transform.position = block.GridRef.GetPosition(_mX, _mY, offset);
+    }
+}
