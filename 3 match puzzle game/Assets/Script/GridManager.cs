4bcd1e6d55b1085ea3e5ef2eff753e9d7a584b5e
Mooooon-s <mmk990212@naver.com>
2025-02-06 00:48:39 +0900

    FEAT: 블럭 생성

--- 3 match puzzle game/Assets/Script/GridManager.cs
+++ 3 match puzzle game/Assets/Script/GridManager.cs
@@ -1,14 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class GridManager : MonoBehaviour
 {
     public static GridManager Instance = null;
-    
+    public enum BlockType
+    {
+        Normal,
+        Active,
+        Obstacle,
+        End
+    }
+
+    [System.Serializable]
+    public struct Block
+    {
+        public BlockType _type;
+        public GameObject _block;
+
+    }
+
+    public Block[] blockPrefabs;
+
+    private Dictionary<BlockType, GameObject> blockPrefabDict;
     public GameObject Grid;
     public int xSize,ySize;
-    public GameObject[,] tiles;
+    public GameObject[,] Blocks;
+
 
     public List<Sprite> list = new List<Sprite>();
 
@@ -16,13 +36,21 @@ public class GridManager : MonoBehaviour
     {
         Instance = GetComponent<GridManager>();
         Vector2 offset = Grid.GetComponent<SpriteRenderer>().bounds.size;
-        //Debug.Log(offset);
-        CreateGrid(offset.x,offset.y);
+
+        blockPrefabDict = new Dictionary<BlockType, GameObject>();
+        for(int i = 0; i < blockPrefabs.Length; i++)
+        {
+            if (!blockPrefabDict.ContainsKey(blockPrefabs[i]._type))
+            {
+                blockPrefabDict.Add(blockPrefabs[i]._type, blockPrefabs[i]._block);
+            }
+        }
+        CreateGrid(offset);
     }
 
-    private void CreateGrid(float offsetX, float offsetY)
+    private void CreateGrid(Vector2 _offset)
     {
-        tiles = new GameObject[xSize, ySize];
+        Blocks = new GameObject[xSize, ySize];
 
         Vector3 StartPos = transform.position;
 
@@ -30,11 +58,22 @@ public class GridManager : MonoBehaviour
         {
             for(int j = 0; j < ySize; j++)
             {
+                //Create Grid
                 GameObject NewGrid = 
-                    Instantiate<GameObject>(Grid, new Vector3(StartPos.x+(i*offsetX), StartPos.y+(j*offsetY)),Grid.transform.rotation);
+                    Instantiate<GameObject>(Grid, GetPosition(i, j, _offset),Quaternion.identity);
             }
         }
 
+        for(int i = 0;i < xSize; i++)
+        {
+            for(int j = 0;j<ySize; j++)
+            {
+                //Create Block
+                GameObject NewBlock =
+                    Instantiate<GameObject>(blockPrefabDict[BlockType.Normal], GetPosition(i, j, _offset), Quaternion.identity) ;
+                NewBlock.name =NewBlock.name+ " (" + i + ", " + j+")";
+            }
+        }
         /*for(int i = 0;i < xSize; i++)
         {
             for(int j= 0; j < ySize; j++)
@@ -42,6 +81,11 @@ public class GridManager : MonoBehaviour
                 Debug.Log(tiles[i,j].name);
             }
         }*/
+    }
+
+    Vector2 GetPosition(float _x, float _y,Vector2 _offset)
+    {
+        return new Vector2(transform.position.x - xSize/2.0f +_x*(_offset.x), transform.position.y + ySize/2.0f -_y*(_offset.y));
     }
 
     // Start is called before the first frame update
